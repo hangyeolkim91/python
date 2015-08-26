8031b16522f3ecb34a7f202bf03a94d1b2f96995
hangyeolkim91 <hangyeoulkim@gmail.com>
2015-08-26 16:36:52 +0900

    server forever

--- server.py
+++ server.py
@@ -1,10 +1,31 @@
-from socket import *
+from socketserver import ThreadingTCPServer,StreamRequestHandler
 
-svrSock = socket(AF_INET,SOCK_STREAM)
-svrSock.bind(('127.0.0.1',2000))
-svrSock.listen(1)
-conn, addr = svrSock.accept()
+PORT = 2000
 
-recvBuf = conn.recv(1024)
-print(len(recvBuf))
-print(recvBuf)
+class MyRequestHandler(StreamRequestHandler):
+    def handle(self):
+        conn = self.request
+        print('connection from',self.client_address)
+        buf = conn.recv(1024)
+        if not buf:
+            print('nothing')
+        else:
+            print(buf)
+
+server = ThreadingTCPServer(('127.0.0.1',2000),MyRequestHandler)
+
+print('listening on port', PORT)
+
+server.serve_forever()
+
+
+
+
+# svrSock = socket(AF_INET,SOCK_STREAM)
+# svrSock.bind(('127.0.0.1',2000))
+# svrSock.listen(1)
+# conn, addr = svrSock.accept()
+#
+# recvBuf = conn.recv(1024)
+# print(len(recvBuf))
+# print(recvBuf)
